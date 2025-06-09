4013f6656e2f4d4142b58b5362dc03fc68623d46
HyeonGeol <boo1996@naver.com>
2025-06-09 21:07:51 +0900

    feat: 은행 별 전략 모듈에 provider로 등록

--- src/scrapers/scrapers.module.ts
+++ src/scrapers/scrapers.module.ts
@@ -4,6 +4,19 @@ import { BankScraperService } from './bank/bank-scraper.service';
 import { CaptchaService } from './captcha/captcha.service';
 import { ScrapingStrategyFactory } from './strategies/scraping-strategy.factory';
 import { UtilsModule } from 'src/utils/utils.module';
+import { KbBankStrategy } from './bank/strategies/kb-bank.strategy';
+import { HanaBankStrategy } from './bank/strategies/hana-bank.strategy';
+import { IbkBankStrategy } from './bank/strategies/ibk-bank.strategy';
+import { WooriBankStrategy } from './bank/strategies/woori-bank.strategy';
+import { ShinhanBankStrategy } from './bank/strategies/shinhan-bank.strategy';
+
+const strategies = {
+  kb: new KbBankStrategy(),
+  hana: new HanaBankStrategy(),
+  ibk: new IbkBankStrategy(),
+  woori: new WooriBankStrategy(),
+  shinhan: new ShinhanBankStrategy(),
+};
 
 @Module({
   imports: [
@@ -12,7 +25,15 @@ import { UtilsModule } from 'src/utils/utils.module';
     }),
     UtilsModule,
   ],
-  providers: [BankScraperService, CaptchaService, ScrapingStrategyFactory],
+  providers: [
+    BankScraperService,
+    CaptchaService,
+    ScrapingStrategyFactory,
+    {
+      provide: 'BANK_STRATEGY_MAP',
+      useValue: strategies,
+    },
+  ],
   exports: [BankScraperService, CaptchaService, ScrapingStrategyFactory],
 })
 export class ScrapersModule {}
